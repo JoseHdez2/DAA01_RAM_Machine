54adc00e0cf7e63e61f035d3efc4abc3b9a572d0
Jose Hernandez <josemanuelhdez2@gmail.com>
2016-02-22 18:43:14 +0000

    InstructionParser#parseLineAsInstruction().
    Stopped to laugh at old code.

--- src/cur/auto/ram/Instruction.java
+++ src/cur/auto/ram/Instruction.java
@@ -51,25 +51,6 @@ public class Instruction {
         validOperands.put(InsType.HALT, 
                 new ArrayList<OpType>(Arrays.asList(OpType.NONE)));
     }
-            
-    
-    // Instructions that can take no operands.
-    static ArrayList<InsType> INST_TAKES_NONE = 
-            new ArrayList<InsType>(Arrays.asList(InsType.HALT));
-    
-    // Instructions that can take a direct or indirect (numeric) operand.
-    static ArrayList<InsType> INST_TAKES_NUM_DIR_IND = 
-            new ArrayList<InsType>(Arrays.asList(InsType.LOAD, InsType.STORE, InsType.READ, 
-                    InsType.WRITE, InsType.ADD, InsType.SUB, InsType.MUL, InsType.DIV));
-    
-    // Instructions that can take a numeric literal operand.
-    static ArrayList<InsType> INST_TAKES_NUM_LIT = 
-            new ArrayList<InsType>(Arrays.asList(InsType.LOAD,
-                    InsType.WRITE, InsType.ADD, InsType.SUB, InsType.MUL, InsType.DIV));
-    
-    // Instructions that can take a string (name) operand.
-    static ArrayList<InsType> INST_TAKES_NAME = 
-        new ArrayList<InsType>(Arrays.asList(InsType.JUMP, InsType.JZERO, InsType.JGTZ));
     
     enum OpType {
         NONE,           // No operand.
@@ -84,62 +65,17 @@ public class Instruction {
     String op;  // Instruction operand. Either one or none.
     
     /**
-     * Given a string that represents an operand, return the operand type.
-     * @param str String that represents an operand.
-     * @return OpType, null if no matching type was found.
-     */
-    public static OpType operandType(String str){
-//        if (str.matches("[=*]?\\d+"));
-        if (str.matches("\\d+")) return OpType.NUM_DIRECT;
-        if (str.matches("=\\d+")) return OpType.NUM_LITERAL;
-        if (str.matches("*\\d+")) return OpType.NUM_INDIRECT;
-        if (str.matches("[A-z_]+")) return OpType.NAME;
-        if (str.matches("") || str == null) return OpType.NONE;
-        else {
-            System.err.println(str + ": Unknown operand type.");
-            return null;    // return NONE?
-        }
-    }
-    
-    /**
      * Check validity of operand, relative to the type of this instruction.
      * ("Is my operand compatible with me?")
      */
     private boolean operandValidityCheck(){
-        // New code.
+        assert(opType != null);
         
-        // vv Most important line in this class. vv
+        // "If the operand type is not within the valid types for this instruction type..."
         if (!validOperands.get(insType).contains(opType)){
-            String err = String.format("%s: invalid %s operand.", insType, opType);
+            System.err.println(String.format("%s: invalid %s operand.", insType, opType));
+            return false;
         } else return true;
-        
-        // Old code.
-        
-        // We assume Instruction#operandType(op) == opType. InsParser did its job.
-        switch(opType){
-        case NAME:
-            if (!INST_TAKES_NAME.contains(insType)){
-            System.err.println(insName + ": invalid name operand."); return false; }
-            else return true;
-        case NONE:
-            if (!INST_TAKES_NONE.contains(insType)){
-            System.err.println(insName + ": needs an operand."); return false; }
-            else return true;
-        case NUM_DIRECT:
-            if (!INST_TAKES_NUM_DIR_IND.contains(insType)){ 
-            System.err.println(insName + ": needs an operand."); return false; }
-            else return true;
-        case NUM_INDIRECT:
-            if (!INST_TAKES_NUM_DIR_IND.contains(insType)){
-            System.err.println(insName + ": needs an operand."); return false; }
-            else return true;
-        case NUM_LITERAL:
-            if (!INST_TAKES_NONE.contains(insType)){
-            System.err.println(insName + ": needs an operand."); return false; }
-            else return true;
-        }
-        System.err.println("Null opType or something? error.");
-        return false;
     }
     
     /**
--- src/cur/auto/ram/InstructionParser.java
+++ src/cur/auto/ram/InstructionParser.java
@@ -1,5 +1,8 @@
 package cur.auto.ram;
 
+import cur.auto.ram.Instruction.InsType;
+import cur.auto.ram.Instruction.OpType;
+
 /**
  * Instruction knows what an instruction can be.
  * RAM_Auto knows how each instruction behaves.
@@ -9,5 +12,90 @@ package cur.auto.ram;
  *
  */
 public class InstructionParser {
+    
+    
+    /**
+     * @param str
+     * @return
+     */
+    private static InsType instructionType(String str){
+        
+    }
+    
+    /**
+     * Given a string that represents an operand, return the operand type.
+     * @param str String that represents an operand.
+     * @return OpType, null if no matching type was found.
+     */
+    private static OpType operandType(String str){
+//        if (str.matches("[=*]?\\d+"));
+        if (str.matches("\\d+")) return OpType.NUM_DIRECT;
+        if (str.matches("=\\d+")) return OpType.NUM_LITERAL;
+        if (str.matches("*\\d+")) return OpType.NUM_INDIRECT;
+        if (str.matches("[A-z_]+")) return OpType.NAME;
+        if (str.matches("") || str == null) return OpType.NONE;
+        else {
+            System.err.println(str + ": Unknown operand type.");
+            return null;    // return NONE?
+        }
+    }
+    
+    /**
+     * Main purpose of this class.
+     * Parse, create and return the instruction represented by the provided line.
+     * @param line String that contains an instruction.
+     * @return Instruction.
+     */
+    public Instruction parseLineAsInstruction(String line){
+     // Do actual parsing
+        for (int i = 0; i < lines2.size(); i++){
+            String dummy = lines2.get(i); // split() is destructive.
+            String[] thisLine = dummy.split(SEP);
+            System.out.println(lines2.get(i));
+            System.out.println(String.format("thisLine[1]: '%s'", thisLine[1]));
+            // Detect tags/"goto"s and add them
+            if (thisLine.length > 1 && thisLine[1].equals(":")){
+                System.out.println("doopy woop!");
+                gotos.put(thisLine[0], i);  // Add goto with key= token 0 and value= this line's index.
+                dummy = lines2.get(i); // split() is destructive.
+                thisLine = dummy.substring(dummy.indexOf(':')+2).split(SEP);
+//                thisLine = dummy.split(":")[1].split(SEP);  // 'Remove' tag/goto.
+            }
+            
+            // Parse instruction and optional operand
+            System.out.println(String.format("thisLine: '%s'", thisLine));
+            assert(thisLine.length <= 2);
 
+            // throws IllegalArgumentException if instruction is not defined in Instruction.InsType.
+            Instruction.InsType insType = InsType.valueOf(thisLine[0].toUpperCase());
+            
+            // WOW, I AM SO SMART! THAT WAS SMART. WHAT I DID BELOW AND WAS THE SAME AS LINE 70.
+            /*
+            switch(Instruction.InsType.valueOf(thisLine[0].toUpperCase())){
+            case ADD: insType = Instruction.InsType.ADD; break;
+            case DIV: insType = Instruction.InsType.DIV; break;
+            case HALT:insType = Instruction.InsType.HALT; break;
+            case JGTZ:insType = Instruction.InsType.JGTZ; break;
+            case JUMP:insType = Instruction.InsType.JUMP; break;
+            case JZERO:insType = Instruction.InsType.JZERO; break;
+            case LOAD:insType = Instruction.InsType.LOAD; break;
+            case MUL:insType = Instruction.InsType.MUL; break;
+            case READ:insType = Instruction.InsType.READ; break;
+            case STORE:insType = Instruction.InsType.STORE; break;
+            case SUB:insType = Instruction.InsType.SUB; break;
+            case WRITE:insType = Instruction.InsType.WRITE; break;
+            default:
+                System.err.println("Unrecognized instruction: Halting load process.");
+                break;
+            }
+            */
+            if (thisLine.length == 2){
+                assert(operandType(thisLine[1]) != null); // Check for valid operand syntax.
+                return new Instruction(insType, thisLine[1]);
+            }
+            else return new Instruction(insType);
+        }
+        
+        return new Instruction(InsType.HALT);
+    }
 }
